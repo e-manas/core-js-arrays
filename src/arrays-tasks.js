735dd89fabd507f174adf6bea6d593e112d1506e
Manas Egemberdiev <manas.egemberdiev@gmail.com>
2025-01-10 21:09:51 +0600

    resolve task findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -514,8 +514,21 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  if (!nums.length) return 0;
+  return nums.reduce(
+    (acc, curr, i) => {
+      if (i === 0) return { currentLength: 1, maxLength: 1 };
+      if (curr > nums[i - 1]) {
+        acc.currentLength += 1;
+        acc.maxLength = Math.max(acc.maxLength, acc.currentLength);
+      } else {
+        acc.currentLength = 1;
+      }
+      return acc;
+    },
+    { currentLength: 1, maxLength: 1 }
+  ).maxLength;
 }
 
 /**
